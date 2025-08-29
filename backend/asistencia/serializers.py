b2c9dd2d8a28373d86ed1e23695e9bdb5e2e5e24
frankccajavilca-stack <frankccajavilca@gmail.com>
2025-08-28 20:16:06 -0500

    Agrego validación en serializer para evitar asistencias duplicadas por estudiante y fecha

--- backend/asistencia/serializers.py
+++ backend/asistencia/serializers.py
@@ -4,4 +4,23 @@ from .models import Asistencia
 class AsistenciaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Asistencia
-        fields = "__all__"
+        fields = '__all__'
+
+    def validate(self, data):
+        estudiante = data.get("estudiante")
+        fecha = data.get("fecha")
+
+        # Filtrar si ya existe ese estudiante en esa fecha
+        qs = Asistencia.objects.filter(estudiante=estudiante, fecha=fecha)
+
+        # Si estoy editando, excluir este mismo registro
+        if self.instance:
+            qs = qs.exclude(pk=self.instance.pk)
+
+        if qs.exists():
+            raise serializers.ValidationError(
+                "Ya existe un registro de asistencia para este estudiante en esa fecha."
+            )
+
+        return data
+
